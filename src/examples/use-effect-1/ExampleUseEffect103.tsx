2302e8cf3c9e480412a5a2c390f58c02d6ba03b3
Vassili Rezvoy <vassili.rezvoy@gmail.com>
2020-04-26 23:07:37 +0200

    updated formation

--- src/examples/use-effect-1/ExampleUseEffect103.tsx
+++ src/examples/use-effect-1/ExampleUseEffect103.tsx
@@ -4,74 +4,33 @@ import cloneDeep from "lodash.clonedeep";
 import { useLog } from "../../commons/ExampleBloc";
 
 const SubComponent = ({
-  value,
-  setValue,
-  otherValue,
-  setOtherValue
+  parentValue,
+  setParentValue,
 }: {
-  value: number;
-  setValue: (newValue: number) => void;
-  otherValue: { str: string };
-  setOtherValue: (newValue: { str: string }) => void;
+  parentValue: { str: string };
+  setParentValue: (newValue: { str: string }) => void;
 }) => {
   const log = useLog();
-  const [state, setState] = React.useState<number>(value);
-  const [otherState, setOtherState] = React.useState<any>(otherValue);
+  const [state, setState] = React.useState<any>(parentValue);
   const [shouldSetSuperState, setShouldSetSuperState] = React.useState<boolean>(false);
 
   log("render");
 
   React.useEffect(() => {
-    setState(value);
-  }, [value, setState]);
-  React.useEffect(() => {
     log("use effect");
-    if (shouldSetSuperState) setOtherValue(otherValue);
-    else setOtherState(otherValue);
-  }, [otherValue, setOtherState, setOtherValue, shouldSetSuperState, log]);
+    if (shouldSetSuperState) setParentValue(state);
+    setState(parentValue);
+  }, [parentValue, setParentValue, state, setState, shouldSetSuperState, log]);
 
   return (
     <>
-      <pre>{JSON.stringify({ value, state, otherState, shouldSetSuperState }, null, 2)}</pre>
+      <pre>{JSON.stringify({ state, shouldSetSuperState }, null, 2)}</pre>
       <ul>
-        <li>
-          <ActionButton
-            label="Increment state"
-            onClick={() => {
-              setState(state => state + 1);
-            }}
-          />
-        </li>
-        <li>
-          <ActionButton
-            label="Increment super state"
-            onClick={() => {
-              setValue(value + 1);
-            }}
-          />
-        </li>
         <li>
           <ActionButton
             label="Change other state"
             onClick={() => {
-              setOtherState({ someProps: "zefzefz" });
-            }}
-          />
-        </li>
-        <li>
-          <ActionButton
-            label="Change super other state"
-            onClick={() => {
-              setOtherValue({ str: "zezfez" });
-            }}
-          />
-        </li>
-        <li>
-          <ActionButton
-            label="Reset super state"
-            onClick={() => {
-              setValue(0);
-              setOtherValue({ str: "" });
+              setState({ someProps: "zefzefz" });
             }}
           />
         </li>
@@ -79,7 +38,7 @@ const SubComponent = ({
           <ActionButton
             label="Change state option"
             onClick={() => {
-              setShouldSetSuperState(state => !state);
+              setShouldSetSuperState((state) => !state);
             }}
           />
         </li>
@@ -89,17 +48,9 @@ const SubComponent = ({
 };
 
 const ExampleUseState106 = () => {
-  const [counter, setCounter] = React.useState<number>(0);
   const [str, setStr] = React.useState<{ str: string }>({ str: "" });
 
-  return (
-    <SubComponent
-      value={counter}
-      setValue={setCounter}
-      otherValue={cloneDeep(str)}
-      setOtherValue={setStr}
-    />
-  );
+  return <SubComponent parentValue={cloneDeep(str)} setParentValue={setStr} />;
 };
 
 export default ExampleUseState106;
